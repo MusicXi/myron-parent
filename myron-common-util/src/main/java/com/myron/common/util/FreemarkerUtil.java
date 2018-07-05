d0315866ddbf0874cfcc6e6cb2c252416159861d
MusicXi <792331407@qq.com>
2018-07-05 18:38:03 +0800

    修改版本号:1.0.0-SNAPSHOT

--- myron-common-util/src/main/java/com/myron/common/util/FreemarkerUtil.java
+++ myron-common-util/src/main/java/com/myron/common/util/FreemarkerUtil.java
@@ -1,85 +1,83 @@
 package com.myron.common.util;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.List;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class FreemarkerUtil {
-	private static final Logger LOGGER=LoggerFactory.getLogger(FreemarkerUtil.class);
-	//创建一个freemarker.template.Configuration实例，它是存储 FreeMarker 应用级设置的核心部分
-	private static Configuration configuration=new Configuration(Configuration.VERSION_2_3_22);//指定版本号
-	//private static String templatePath;
-	
-	public FreemarkerUtil(){
-		throw new IllegalAccessError("Utility class");
-	}
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(FreemarkerUtil.class);
+
+    public FreemarkerUtil() {
+        throw new IllegalAccessError("Utility class");
+    }
+
+
     /**
      * 通过模板名称获取模板
-     * @param templateName
+     * @param templateLoaderPath 模板加载目录,例:"classpath:templates/freemarker"
+     * @param templateName 模板名称
      * @return
      * @throws IOException
+     * @throws TemplateException
      */
-    public static Template getFreemarkTemplate(String templateName) throws IOException{
-    	
-        //获得模板文件路径
-        String templatePath=FreemarkerUtil.class.getClassLoader().getResource("/templates/freemarker").getPath();
-        LOGGER.info("FreemarkerUtil模板路径：{}",templatePath);
-        //设置模板目录
-        configuration.setDirectoryForTemplateLoading(new File(templatePath));
-        //设置默认编码格式
-        configuration.setDefaultEncoding("UTF-8");
-        
-        //从设置的目录中获得模板
-        Template template = configuration.getTemplate(templateName);
+    public static Template getFreemarkTemplate(String templateLoaderPath, String templateName) throws IOException, TemplateException {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+
+//       freeMarkerConfigurer.setTemplateLoaderPaths("classpath:templates/java/mybatis","classpath:templates/mybatis");
+        freeMarkerConfigurer.setTemplateLoaderPaths(templateLoaderPath);
+        Properties freemarkerSettings = new Properties();
+        freemarkerSettings.put("default_encoding", "UTF-8");
+        freemarkerSettings.put("locale", "zh_CN");
+        freeMarkerConfigurer.setFreemarkerSettings(freemarkerSettings);
+        freeMarkerConfigurer.afterPropertiesSet();
+
+        Template template = freeMarkerConfigurer.getConfiguration().getTemplate(templateName);
         return template;
     }
-    
+
     /**
      * 生成html文件 (将模板+数据生成的结果写入文件中，得到一个静态文件)
-     * @param file	生成文件的路径
-     * @param template	生成文件的模板
-     * @param data	需要渲染到html的数据
-     * @throws IOException 
-     * @throws TemplateException 
+     *
+     * @param file     生成文件的路径
+     * @param template 生成文件的模板
+     * @param data     需要渲染到html的数据
+     * @throws IOException
+     * @throws TemplateException
      */
-    public static void createHtmlFile(File file, Template template, Map<String, Object> data) throws TemplateException, IOException{
-    	//文件输出流写入器
-    	Writer writer=new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+    public static void createHtmlFile(File file, Template template, Map<String, Object> data) throws TemplateException, IOException {
+        //文件输出流写入器
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
         //将模板+数据生成的结果写入文件中，得到一个静态文件
-    	Writer out = new OutputStreamWriter(System.out);
-    	template.process(data, writer);
-    	template.process(data, out);
+        Writer out = new OutputStreamWriter(System.out);
+        template.process(data, writer);
+        template.process(data, out);
         writer.flush();
         writer.close();
     }
-    
+
     /**
      * 批量生成html文件 (将模板+数据生成的结果写入文件中，得到一个静态文件)
-     * @param file	生成文件的路径
-     * @param template	生成文件的模板
-     * @param list	需要渲染到html的数据列表集合
-     * @throws IOException 
-     * @throws TemplateException 
+     *
+     * @param file     生成文件的路径
+     * @param template 生成文件的模板
+     * @param list     需要渲染到html的数据列表集合
+     * @throws IOException
+     * @throws TemplateException
      */
-    public static void createHtmlFile(File file, Template template, List<Map<String, Object>> list) throws TemplateException, IOException{
-    	//文件输出流写入器
-    	Writer writer=new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+    public static void createHtmlFile(File file, Template template, List<Map<String, Object>> list) throws TemplateException, IOException {
+        //文件输出流写入器
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
         //将模板+数据生成的结果写入文件中，得到一个静态文件
-    	for(Map<String, Object> data: list){
-    		template.process(data, writer);    		
-    	}
+        for (Map<String, Object> data : list) {
+            template.process(data, writer);
+        }
         writer.flush();
         writer.close();
     }
